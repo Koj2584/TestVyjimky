41e303319cabeaef8494039172129fc1e4fc3eff
Koj2584 <73990215+Koj2584@users.noreply.github.com>
2023-06-07 09:01:25 +0200

    Add files via upload

---
+++ TestVyjimky/TestVyjimky/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestVyjimky
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int pocet = 0, preteceniCisla = 0;
+            long soucet = 0;
+            foreach(string row in textBox1.Lines)
+            {
+                try
+                {
+                    if (row[0] == '-')
+                    {
+                        try
+                        {
+                            long cislo = long.Parse(row);
+                            try
+                            {
+                                checked { soucet += cislo; }
+                                pocet++;
+                            }
+                            catch (OverflowException)
+                            {
+                                soucet = long.MinValue;
+                                preteceniCisla++;
+                            }
+                        }
+                        catch (FormatException)
+                        { }
+                        catch (OverflowException)
+                        { }
+                    }
+                } catch(IndexOutOfRangeException)
+                { }
+            }
+            if(preteceniCisla > 0)
+            {
+                MessageBox.Show("Čísla přesáhla velikost datového typu. Nezapočítali se " + preteceniCisla + " čísla.", "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            try
+            {
+                
+                if (pocet == 0)
+                    throw new DivideByZeroException();
+                double vysledek = (double)soucet / pocet;
+                label1.Text = "Průměr: " + vysledek;
+            } catch(DivideByZeroException)
+            {
+                label1.Text = "Průměr: - ? -";
+            }
+        }
+    }
+}
